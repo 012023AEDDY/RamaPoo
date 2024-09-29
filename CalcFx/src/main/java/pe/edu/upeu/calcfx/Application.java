eba84bb75efad4eccaae819257e777ab9aaa0940
012023AEDDY <milkonmamaniccama@gmail.com>
2024-09-29 07:27:39 -0500

    ultimo

--- CalcFx/src/main/java/pe/edu/upeu/calcfx/Application.java
+++ CalcFx/src/main/java/pe/edu/upeu/calcfx/Application.java
@@ -2,5 +2,5 @@ package pe.edu.upeu.calcfx;
 
 public class  Application {
     public static void main(String[] args) {
-        CalcFxApplication.main(args);}//encargada de hacer correr toda la aplicacion
+        CalcFxApplication.main(args);}//encarholadjkgada de hacer correr toda la aplicacion
 }
